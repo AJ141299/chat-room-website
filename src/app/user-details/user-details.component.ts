af46d82464eb1ce518ba6e2bc5d13692b22b3c9f
Abdullah Abdullah <abdullah.abdullah@paymentsense.com>
2023-01-15 05:11:37 +0000

    Added animation

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -3,11 +3,23 @@ import { FormControl } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { setUsername } from '../state/actions/user.actions';
 import { AppState } from '../state/models/models';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.scss']
+  styleUrls: ['./user-details.component.scss'],
+  animations: [
+    trigger('fadeUp', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(20%)' }),
+        animate(
+          '200ms ease',
+          style({ opacity: 1, transform: 'translateY(0%)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class UserDetailsComponent {
   username: FormControl = new FormControl();
